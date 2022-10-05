5a5c512985437c1e10abf2e900c843a985e06ece
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2022-10-05 21:06:01 +0200

    ABC

--- experimental_scripts/experiment2_rev_all.py
+++ experimental_scripts/experiment2_rev_all.py
@@ -1,28 +1,32 @@
 """
-Comparison sytnethic GNB, HT, MLP, revision
+Comparison sytnethic MLP
 """
 import numpy as np
 import strlearn as sl
 import config2_rev_all as config
+from sklearn.base import clone
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+from strlearn.ensembles import WAE
 from strlearn.evaluators import TestThenTrain
-import multiprocessing
+import sys
 
 
 np.set_printoptions(suppress=True)
 replications = config.replications()
-
-def worker(stream):
-    print("Start: %s" % (stream))
-    methods = config.methods()
-    ttt = TestThenTrain(metrics=list(config.metrics().values()), verbose=False)
-    ttt.process(streams[stream], methods)
-    np.save("results/ex2_rev_all/%s" % stream, ttt.scores)
-    print("End: %s" % (stream))
-
-jobs = []
 for replication in range(replications):
+    # Select streams, methods and metrics from config
     streams = config.streams(config.random_state() + replication)
+    print(len(streams))
     for stream in streams:
-        p = multiprocessing.Process(target=worker, args=(stream,))
-        jobs.append(p)
-        p.start()
+        print(stream)
+        methods = config.methods()
+
+        print(len(methods), "methods")
+
+        ttt = TestThenTrain(metrics=list(config.metrics().values()), verbose=True)
+        ttt.process(streams[stream], methods)
+
+        print(ttt.scores.shape)
+
+        np.save("results/ex2_rev_all/%s" % stream, ttt.scores)
